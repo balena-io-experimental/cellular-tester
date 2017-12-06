7265f5cf4c0166f87cb47ae320f31666b82b322e
Shaun Mulligan <shaun@resin.io>
2017-12-06 12:01:47 +0000

    initial

---
+++ src/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Starting Modem Checker script"
+
+# Setting Network Manager bus so that our client can communicate with it
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+# Run connection check script every 1min
+# wait indefinitely
+while :
+do
+	# Log signal quality
+	mmcli -L | grep -q Modem
+	if [ $? -eq 0 ]; then
+		MODEM_NUMBER=`mmcli -L | grep Modem | sed -e 's/\//\ /g' | awk '{print $5}'` 
+		echo `mmcli -m ${MODEM_NUMBER} | grep quality`
+		echo `mmcli -m ${MODEM_NUMBER} --command="AT+CSQ"`
+	fi
+	sleep 60;
+
+done
